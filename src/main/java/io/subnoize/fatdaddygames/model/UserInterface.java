df5228217fd571cb89b6607abc228f9d44684eff
ericb <ericb@Sapphire>
2025-03-19 14:51:38 -0400

    Added Lemur to the project.

--- src/main/java/io/subnoize/fatdaddygames/model/UserInterface.java
+++ src/main/java/io/subnoize/fatdaddygames/model/UserInterface.java
@@ -5,7 +5,12 @@ import org.springframework.stereotype.Component;
 
 import com.jme3.font.BitmapText;
 import com.jme3.math.ColorRGBA;
+import com.jme3.scene.Node;
 import com.jme3.system.AppSettings;
+import com.simsilica.lemur.Button;
+import com.simsilica.lemur.Command;
+import com.simsilica.lemur.Container;
+import com.simsilica.lemur.Label;
 
 import io.subnoize.fatdaddygames.configuration.GameConfiguration;
 
@@ -17,6 +22,9 @@ public class UserInterface {
 
 	@Autowired
 	private AppSettings settings;
+	
+	@Autowired
+	private Node guiNode;
 
 	public BitmapText initHud() {
 		BitmapText hudText = new BitmapText(configuration.guiFont());
@@ -37,6 +45,25 @@ public class UserInterface {
 	}
 
 	public BitmapText initMenu() {
+
+		// Create a simple container for our elements
+		Container myWindow = new Container();
+		guiNode.attachChild(myWindow);
+
+		// Put it somewhere that we will see it.
+		// Note: Lemur GUI elements grow down from the upper left corner.
+		myWindow.setLocalTranslation(300, 300, 0);
+
+		// Add some elements
+		myWindow.addChild(new Label("Hello, World."));
+		Button clickMe = myWindow.addChild(new Button("Click Me"));
+		clickMe.addClickCommands(new Command<Button>() {
+			@Override
+			public void execute(Button source) {
+				System.out.println("The world is yours.");
+			}
+		});
+
 		BitmapText menuText = new BitmapText(configuration.guiFont());
 		menuText.setSize(configuration.guiFont().getCharSet().getRenderedSize() + 10);
 		menuText.setColor(ColorRGBA.White);
