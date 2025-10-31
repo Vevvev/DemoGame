f3870d3e7113d797675f4c7c42c67cacb1659be4
Eric Bryant <eric@subnoize.net>
2025-10-31 15:13:49 -0400

    Added high score object.

---
+++ src/main/java/io/subnoize/fatdaddygames/model/HighScore.java
@@ -0,0 +1,52 @@
+package io.subnoize.fatdaddygames.model;
+
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
+public class HighScore {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String playerName;
+	
+	private int score;
+
+	public HighScore() {
+	}
+
+	public HighScore(String playerName, int score) {
+		this.playerName = playerName;
+		this.score = score;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getPlayerName() {
+		return playerName;
+	}
+
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+}
